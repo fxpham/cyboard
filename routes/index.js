3b3464c04d3367e79735310b9c5ace008e00ea36
Y Pham <yxpham@gmail.com>
2025-05-28 22:27:39 +0700

    Get list executed command based on log files

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+const fs = require('fs');
+const path = require('path');
 const packageJson = require('../package.json');
 
 /* GET home page. */
@@ -11,7 +13,22 @@ router.get('/', function(req, res, next) {
       .filter(cmd => cmd.startsWith('spec:'))
       .sort();
   }
-  res.render('index', { title: 'Express', specCommands });
+
+  // Get executed commands from log files in public/logs
+  const logsDir = path.join(__dirname, '../public/logs');
+  let executedCommands = [];
+  try {
+    if (fs.existsSync(logsDir)) {
+      executedCommands = fs.readdirSync(logsDir)
+        .filter(file => file.endsWith('.log'))
+        .map(file => file.replace('.log', '').replace(/_/g, ':'))
+        .sort();
+    }
+  } catch (e) {
+    // If error, leave executedCommands empty
+  }
+
+  res.render('index', { title: 'Express', specCommands, executedCommands });
 });
 
 module.exports = router;
