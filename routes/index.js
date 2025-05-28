bf5978f6c938fc3f4b9c0beb35292f4d12460016
Y Pham <yxpham@gmail.com>
2025-05-28 21:47:37 +0700

    Render list command start with spec

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+const packageJson = require('../package.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // Get all script commands starting with 'spec:' and sort them
+  let specCommands = [];
+  if (packageJson.scripts) {
+    specCommands = Object.keys(packageJson.scripts)
+      .filter(cmd => cmd.startsWith('spec:'))
+      .sort();
+  }
+  res.render('index', { title: 'Express', specCommands });
 });
 
 module.exports = router;
