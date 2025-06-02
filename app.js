14cf1354ceee2dd1997892b420854fe8ef9506fa
Y Pham <yxpham@gmail.com>
2025-06-02 20:48:29 +0700

    Remove cookie-parser

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
 
 const indexRouter = require('./routes/index');
 const commandRouter = require('./routes/command');
@@ -15,7 +14,6 @@ app.set('view engine', 'hbs');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
