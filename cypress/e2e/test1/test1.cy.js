6f38de2828c95cbfb9b63b44928a9be4ab28879f
Y Pham <yxpham@gmail.com>
2025-06-07 21:29:56 +0700

    Remove cypress

--- cypress/e2e/test1/test1.cy.js
+++
@@ -1,7 +0,0 @@
-describe('Test 1', () => {
-  it('Test 1', () => {
-    cy.visit('https://example.cypress.io');
-    cy.wait(3000);
-    cy.screenshot('test1-screenshot');
-  })
-})
--- cypress/e2e/test3/test3.cy.js
+++
@@ -1,7 +0,0 @@
-describe('Test 3', () => {
-  it('Test 3', () => {
-    cy.visit('https://example.cypress.io');
-    cy.wait(6000);
-    cy.screenshot('test3-screenshot');
-  })
-})
