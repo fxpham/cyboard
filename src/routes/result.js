7e900a2165a4509e0e23759b827f410924d1a533
Y Pham <yxpham@gmail.com>
2025-06-08 12:10:08 +0700

    Update structure

--- src/routes/result.js
+++ src/routes/result.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 const fs = require('fs');
-const scanImages = require('./scanImages');
+const scanImages = require('../utils/util');
 const archiver = require('archiver');
 
 /* GET result of executed command. */
