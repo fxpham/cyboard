619bcfe3ef68c5d6c397da0667ba0ed64d7699f9
Y Pham <yxpham@gmail.com>
2025-06-10 20:58:02 +0700

    Error handler and logger

--- src/app.js
+++ src/app.js
@@ -2,13 +2,16 @@ const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 
+const errorHandler = require('./middlewares/error-handler');
+const logger = require('./middlewares/logger');
+
 const indexRouter = require('./routes/index');
 const commandRouter = require('./routes/command');
 const resultRouter = require('./routes/result');
 
 const app = express();
 
-// view engine setup
+// View engine setup.
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
@@ -21,20 +24,14 @@ app.use('/', indexRouter);
 app.use('/command', commandRouter);
 app.use('/result', resultRouter);
 
-// catch 404 and forward to error handler
+// Catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+// Error handler.
+app.use(errorHandler);
+// Logger.
+app.use(logger);
 
 module.exports = app;
