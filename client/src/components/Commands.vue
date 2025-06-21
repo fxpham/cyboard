040378269eb1ad82616dcbf0747f2a740155f181
Y Pham <yxpham@gmail.com>
2025-06-21 15:13:11 +0700

    Implement filter commands

--- client/src/components/Commands.vue
+++ client/src/components/Commands.vue
@@ -1,28 +1,39 @@
 <script setup>
+import { ref, computed } from 'vue';
 
-defineProps({
+const props = defineProps({
   title: String,
-  data: [],
-})
+  data: Array,
+});
 
+const filter = ref('');
+const filteredCommands = computed(() => {
+  if (!filter.value) return props.data;
+  return props.data.filter(cmd =>
+    cmd.toLowerCase().includes(filter.value.toLowerCase())
+  );
+});
 </script>
 
 <template>
   <div class="column column-1">
     <h2>{{ title }}</h2>
-    <div id="progress-info" class="progress-info">
+    <!-- <div id="progress-info" class="progress-info">
       <p><span id="progress-status"></span></p>
       <p><span id="progress-running"></span></p>
-    </div>
+    </div> -->
     <div class="command-filter-wrap">
       <input type="text" id="command-filter-input" class="command-filter-input"
-        placeholder="Filter commands...">
+        placeholder="Filter commands..." v-model="filter">
     </div>
     <div id="command-list">
       <ul class="custom-list">
-        <li v-for="item in data" class="custom-list-item">
+        <li v-if="filteredCommands.length === 0" class="custom-list-item">
+          <span>Not found</span>
+        </li>
+        <li v-for="item in filteredCommands" :key="item" class="custom-list-item">
           <span>{{ item }}</span>
-          <button class="run-spec-command" data-command="{{ item }}">Run</button>
+          <button class="run-spec-command" :data-command="item">Run</button>
         </li>
       </ul>
     </div>
