a2145960865959ed499a1afba1a23793d3789f80
Y Pham <yxpham@gmail.com>
2025-06-21 15:58:07 +0700

    Integrate vuetify

--- client/src/main.js
+++ client/src/main.js
@@ -1,5 +1,19 @@
 import { createApp } from 'vue'
-import './style.css'
+// import './style.css'
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const vuetify = createVuetify({
+  components,
+  directives,
+  theme: {
+    defaultTheme: 'dark',
+  }
+})
+
+createApp(App).use(vuetify).mount('#app')
