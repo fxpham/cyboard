cdb5f87dfbf8436a17e5db61d2ebc5585f3c39e5
Y Pham <yxpham@gmail.com>
2025-07-05 12:06:25 +0700

    Remove view engine hbs

--- src/app.js
+++ src/app.js
@@ -11,10 +11,6 @@ const resultRouter = require('./routes/result');
 
 const app = express();
 
-// View engine setup.
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '../client/dist')));
